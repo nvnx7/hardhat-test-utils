8d74610a2f47ca916209bd46d734342bc05eb667
theNvN <thenvnsahu@gmail.com>
2022-04-10 21:14:54 +0530

    added time utils

---
+++ src/internal/time.ts
@@ -0,0 +1,44 @@
+import { HardhatEthers } from '../types';
+
+function TimeUtils(ethers: HardhatEthers) {
+  const latest = (): Promise<number> => ethers.provider.getBlock('latest').then(b => b.timestamp);
+
+  const increase = async (sec: number): Promise<number> => {
+    const n = parseInt(`${sec}`);
+    if (isNaN(n)) {
+      throw Error(`Invalid number of seconds argument: ${sec}`);
+    }
+
+    const v = await ethers.provider.send('evm_increaseTime', [n]).then(parseInt);
+    await ethers.provider.send('hardhat_mine', []);
+    return v;
+  };
+
+  const increaseTo = async (timestamp: number): Promise<number> => {
+    const n = parseInt(`${timestamp}`);
+    if (isNaN(n)) {
+      throw Error(`Invalid timestamp argument: ${timestamp}`);
+    }
+
+    const v = await ethers.provider.send('evm_setNextBlockTimestamp', [timestamp]).then(parseInt);
+    await ethers.provider.send('hardhat_mine', []);
+    return v;
+  };
+
+  const duration = {
+    minutes: (n: number) => n * 60,
+    hours: (n: number) => n * 60 * 60,
+    days: (n: number) => n * 60 * 60 * 24,
+    weeks: (n: number) => n * 60 * 60 * 24 * 7,
+    years: (n: number) => n * 60 * 60 * 24 * 365,
+  };
+
+  return {
+    latest,
+    increase,
+    increaseTo,
+    duration,
+  };
+}
+
+export default TimeUtils;
