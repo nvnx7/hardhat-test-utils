bc01c71f636a20b9ca1e783f899de82dfa03af15
theNvN <thenvnsahu@gmail.com>
2022-04-11 00:26:26 +0530

    added a constant

--- src/internal/constants.ts
+++ src/internal/constants.ts
@@ -5,6 +5,7 @@ function Constants(ethers: HardhatEthers) {
     ZERO_ADDRESS: ethers.constants.AddressZero,
     ZERO_BYTES32: ethers.constants.HashZero,
     MAX_UINT256: ethers.constants.MaxUint256,
+    WEI_PER_ETHER: ethers.constants.WeiPerEther,
   };
 }
 
