82a8b44ecd1511c80d3fcfc111638ee4fbf9bb68
theNvN <thenvnsahu@gmail.com>
2022-04-10 21:16:05 +0530

    added BigNumber and TimeUtils to test utils

--- src/TestUtils.ts
+++ src/TestUtils.ts
@@ -1,8 +1,10 @@
 import { HardhatEthers, ITestUtils } from './types';
-import { BlockUtils } from './internal';
+import { BlockUtils, TimeUtils } from './internal';
 
 export function TestUtils(ethers: HardhatEthers): ITestUtils {
   return {
+    BN: ethers.BigNumber,
     block: BlockUtils(ethers),
+    time: TimeUtils(ethers),
   };
 }
