5da90c38b5fe1b757d038c04cb9eb8f487541693
theNvN <thenvnsahu@gmail.com>
2022-04-10 21:13:46 +0530

    added checks and flexibility to types

--- src/internal/block.ts
+++ src/internal/block.ts
@@ -1,19 +1,28 @@
-import { Block, HardhatEthers } from '../types';
+import { Block, HardhatEthers, BigNumber } from '../types';
 
 function BlockUtils(ethers: HardhatEthers) {
   const {
     utils: { hexValue },
+    BigNumber: BN,
   } = ethers;
 
   const isAutomine = (): Promise<boolean> => {
     return ethers.provider.send('hardhat_getAutomine', []);
   };
 
-  const setAutomine = (enabled: boolean): Promise<any> => {
-    return ethers.provider.send('evm_setAutomine', [enabled]);
+  const setAutomine = (enabled: boolean): Promise<boolean> => {
+    return ethers.provider.send('evm_setAutomine', [!!enabled]);
   };
 
-  const setIntervalMining = (ms: number): Promise<any> => {
+  const setIntervalMining = (ms: number | BigNumber): Promise<boolean> => {
+    if (BN.isBigNumber(ms)) {
+      ms = ms.toNumber();
+    }
+
+    if (ms < 0) {
+      throw Error(`Cannot set negative interval mining: ${ms}`);
+    }
+
     return ethers.provider.send('evm_setIntervalMining', [ms]);
   };
 
@@ -25,12 +34,41 @@ function BlockUtils(ethers: HardhatEthers) {
     return ethers.provider.getBlockNumber();
   };
 
-  const advanceBlockBy = (n: number, interval: number | string = 60): Promise<any> => {
+  const advanceBlock = (
+    n: number | BigNumber = 1,
+    interval: number | BigNumber = 60,
+  ): Promise<boolean> => {
+    if (BN.isBigNumber(n)) {
+      n = n.toNumber();
+    }
+
+    if (BN.isBigNumber(interval)) {
+      interval = interval.toNumber();
+    }
+
+    if (n < 0) {
+      throw Error(`Cannot advance negative blocks: ${n}`);
+    }
+
     return ethers.provider.send('hardhat_mine', [hexValue(n), hexValue(interval)]);
   };
 
-  const advanceBlock = (s: number | string = 60): Promise<any> => {
-    return advanceBlockBy(1, s);
+  const advanceBlockTo = async (
+    target: number | BigNumber,
+    interval: number | BigNumber = 60,
+  ): Promise<boolean> => {
+    if (BN.isBigNumber(target)) {
+      target = target.toNumber();
+    }
+
+    const latest = await latestBlockNumber();
+    if (target <= latest) {
+      throw Error(
+        `Cannot advance to the block number ${target} which is not greater that current block number ${latest}`,
+      );
+    }
+
+    return advanceBlock(target - latest, interval);
   };
 
   return {
@@ -38,8 +76,8 @@ function BlockUtils(ethers: HardhatEthers) {
     setAutomine,
     latest,
     latestBlockNumber,
-    advanceBlockBy,
     advanceBlock,
+    advanceBlockTo,
     setIntervalMining,
   };
 }
